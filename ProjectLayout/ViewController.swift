89f5af4062ad2224fcd25f568036eb09c44f5ad5
enes <eng.eneskaya@gmail.com>
2022-05-04 17:12:57 +0300

    Initial Commit

---
+++ ProjectLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Enes Kaya on 4.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
